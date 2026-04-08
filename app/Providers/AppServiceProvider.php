6bb9e7c68a6ef2634b62c4f2a58bc6e92f8c325e
John Lampa <jklampa31@gmail.com>
2026-04-08 21:33:39 +0800

    fix: force https url generation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+
+            $appUrl = config('app.url');
+            if (is_string($appUrl) && str_starts_with($appUrl, 'https://')) {
+                URL::forceRootUrl($appUrl);
+            }
+        }
     }
 }
